79adae66d66cf79261b71b09db051c09c1945208
justyeh <yewenxiang@300.cn>
2020-08-27 09:11:28 +0800

    code

--- backend/models/Dept.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Dept struct {
-	ID       string `json="id"  binding:"required"`
-	Name     string `json="name"  binding:"required"`
-	Status   string `json="sort"  binding:"required"`
-	CreateAt string `json:"createAt"`
-	UpdateAt string `json:"updateAt"`
-}
--- backend/models/Job.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Job struct {
-	ID       string `json="id"  binding:"required"`
-	Name     string `json="name"  binding:"required"`
-	Sort     string `json="sort"  binding:"required"`
-	CreateAt string `json:"createAt"`
-	UpdateAt string `json:"updateAt"`
-}
