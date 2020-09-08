f9624321177f70d3040f8aca2a9ba0be985cc060
justyeh <yewenxiang@300.cn>
2020-09-08 16:43:10 +0800

    修复bug：当icon不存在时页面报错

--- front/src/components/AntIcon.jsx
+++ front/src/components/AntIcon.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import * as Icon from '@ant-design/icons'
 export default ({ name, size = 16 }) => {
-  return React.createElement(Icon[name], {
-    style: { fontSize: size }
-  })
+  return Icon[name]
+    ? React.createElement(Icon[name], {
+        style: { fontSize: size }
+      })
+    : null
 }
